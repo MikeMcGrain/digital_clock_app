f9323f6b9cd4915c52d74aed39e1fcd086bfb556
Mike McGrain <mcgrain.mike@gmail.com>
2020-01-08 09:14:17 -0600

    css and style corrections

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+
 // toggles time between 12 and 24-hour formats
 let twelveHourFormat = true
 const toggleButton = document.getElementById("toggle-time-btn")
@@ -8,22 +11,17 @@ toggleButton.onclick = () => {
 }
 
 //get current time and date info, then display
-function getCurrentTime() {
-    // get current date info, then display
-    let currentDateInfo = new Date()
-
-    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-    let months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-
-    let day = days[currentDateInfo.getDay()]
-    let currentDate = currentDateInfo.getDate()
-    let month = months[currentDateInfo.getMonth()]
-    let year = currentDateInfo.getFullYear()
+function getDateAndTime() {
+    let date = new Date()
+    
+    let day = days[date.getDay()]
+    let currentDate = date.getDate()
+    let month = months[date.getMonth()]
+    let year = date.getFullYear()
 
     document.getElementById("show-day").innerHTML = day
     document.getElementById("show-date").innerHTML = `${month} ${currentDate}, ${year}`
 
-    let date = new Date()
     let hour = twelveHourFormat ? ((date.getHours() + 11) % 12 + 1) : formatTime(date.getHours()) 
     let minute = formatTime(date.getMinutes())
     let second = formatTime(date.getSeconds())
@@ -36,4 +34,5 @@ function formatTime(time) {
     return ("0" + time).slice(-2)
 }
 
-setInterval(getCurrentTime, 1000)
+getDateAndTime()
+setInterval(getDateAndTime, 1000)
