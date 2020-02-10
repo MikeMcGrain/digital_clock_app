973c8a0056e278f74e658ea15b300fe5049748bb
Mike McGrain <mcgrain.mike@gmail.com>
2020-02-10 14:15:15 -0600

    remove am/pm from 24-hour time format

--- app.js
+++ app.js
@@ -2,38 +2,31 @@ const DAYS = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Satur
 const MONTHS = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
 let twelveHourFormat = true
-let toggleButton = document.getElementById("toggle-time-button")
-toggleButton.innerHTML = "24-Hour Clock"
 
-toggleButton.addEventListener("click", () => {
+document.getElementById("toggle-time-button").addEventListener("click", () => {
   twelveHourFormat = !twelveHourFormat
-  toggleButton.innerHTML = twelveHourFormat ? "12-Hour Clock" : "24-Hour Clock"
+  document.getElementById("toggle-time-button").innerText = twelveHourFormat ? "12-Hour Clock" : "24-Hour Clock"
+  renderDateAndTime()
 })
 
-function getDateAndTime() {
-  let dateObject = new Date()
+function renderDateAndTime() {
+  let date = new Date()
 
-  let day = DAYS[dateObject.getDay()]
-  let date = dateObject.getDate()
-  let month = MONTHS[dateObject.getMonth()]
-  let year = dateObject.getFullYear()
+  let hour = twelveHourFormat ? ((date.getHours() + 11) % 12) + 1 : formatTime(date.getHours())
+  let minute = formatTime(date.getMinutes())
+  let second = formatTime(date.getSeconds())
+  let ampm = date.getHours() >= 12 ? "pm" : "am"
 
-  document.getElementById("day").innerHTML = day
-  document.getElementById("date").innerHTML = `${month} ${date}, ${year}`
-
-  let hour = twelveHourFormat
-    ? ((dateObject.getHours() + 11) % 12) + 1
-    : formatTime(dateObject.getHours())
-  let minute = formatTime(dateObject.getMinutes())
-  let second = formatTime(dateObject.getSeconds())
-  let ampm = dateObject.getHours() >= 12 ? "pm" : "am"
-
-  document.getElementById("time").innerHTML = `${hour}:${minute}:${second} ${ampm}`
+  document.getElementById("day").innerText = DAYS[date.getDay()]
+  document.getElementById("date").innerText = `${MONTHS[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
+  document.getElementById("time").innerText = twelveHourFormat 
+    ? `${hour}:${minute}:${second} ${ampm}` 
+    : `${hour}:${minute}:${second}`
 }
 
 function formatTime(time) {
   return ("0" + time).slice(-2)
 }
 
-getDateAndTime()
-setInterval(getDateAndTime, 1000)
+renderDateAndTime()
+setInterval(renderDateAndTime, 1000)
