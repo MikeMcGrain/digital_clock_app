8ee520c99a03eca2a0f276b7fbd062ecaee54fe3
Mike McGrain <mcgrain.mike@gmail.com>
2020-02-10 09:12:03 -0600

    refactor

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+const DAYS = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+const MONTHS = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
-// toggles time between 12 and 24-hour formats
 let twelveHourFormat = true
-const toggleButton = document.getElementById("toggle-time-btn")
+let toggleButton = document.getElementById("toggle-time-button")
 toggleButton.innerHTML = "24-Hour Clock"
-toggleButton.onclick = () => {
-    twelveHourFormat = !twelveHourFormat
-    toggleButton.innerHTML = twelveHourFormat ? "12-Hour Clock" : "24-Hour Clock"
-}
 
-//get current time and date info, then display
+toggleButton.addEventListener("click", () => {
+  twelveHourFormat = !twelveHourFormat
+  toggleButton.innerHTML = twelveHourFormat ? "12-Hour Clock" : "24-Hour Clock"
+})
+
 function getDateAndTime() {
-    let date = new Date()
-    
-    let day = days[date.getDay()]
-    let currentDate = date.getDate()
-    let month = months[date.getMonth()]
-    let year = date.getFullYear()
+  let dateObject = new Date()
 
-    document.getElementById("show-day").innerHTML = day
-    document.getElementById("show-date").innerHTML = `${month} ${currentDate}, ${year}`
+  let day = DAYS[dateObject.getDay()]
+  let date = dateObject.getDate()
+  let month = MONTHS[dateObject.getMonth()]
+  let year = dateObject.getFullYear()
 
-    let hour = twelveHourFormat ? ((date.getHours() + 11) % 12 + 1) : formatTime(date.getHours()) 
-    let minute = formatTime(date.getMinutes())
-    let second = formatTime(date.getSeconds())
-    let ampm = date.getHours() >= 12 ? 'pm' : 'am'
+  document.getElementById("day").innerHTML = day
+  document.getElementById("date").innerHTML = `${month} ${date}, ${year}`
 
-    document.getElementById("show-time").innerHTML = `${hour}:${minute}:${second} ${ampm}` 
+  let hour = twelveHourFormat
+    ? ((dateObject.getHours() + 11) % 12) + 1
+    : formatTime(dateObject.getHours())
+  let minute = formatTime(dateObject.getMinutes())
+  let second = formatTime(dateObject.getSeconds())
+  let ampm = dateObject.getHours() >= 12 ? "pm" : "am"
+
+  document.getElementById("time").innerHTML = `${hour}:${minute}:${second} ${ampm}`
 }
 
 function formatTime(time) {
-    return ("0" + time).slice(-2)
+  return ("0" + time).slice(-2)
 }
 
 getDateAndTime()
